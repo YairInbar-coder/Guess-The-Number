2c83919d67d7e3f68a88146cacea08056a12731c
Yair Inbar <yair.inbar.mail@gmail.com>
2026-02-12 21:40:08 +0200

    a

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -6,26 +6,39 @@ ctk.set_appearance_mode('light')
 ctk.set_default_color_theme('blue')
 
 def temporery_label(parent, text, duration_ms=1000):
-    label = ctk.CTkLabel(parent, text=random.randint(1, 10))
-    label.place(x=126, y=152)
+    label = ctk.CTkLabel(parent, text=text)
+    label.place(x=114, y=152)
     parent.after(duration_ms, label.destroy)
     return label
+
+def check_guess(guess, number):
+    if guess == number:
+        return "Correct!"
+    elif guess < number:
+        return "Too low!"
+    else:
+        return "Too high!"
+
 def main():
     app = ctk.CTk()
     app.title("Guess The Number")
     app.geometry('400x250')
-
-    label = ctk.CTkLabel(app, text="Guess The Number between 1 and 10")
+    number = random.randint(1, 100)
+    label = ctk.CTkLabel(app, text="Guess The Number between 1 and 100")
     label.pack(pady= 30)
 
     text = ctk.CTkTextbox(app, height=30, width=50, fg_color='darkgray')
     text.place(x=175, y=90)
 
-    enter = ctk.CTkButton(app, text="Enter", command=lambda: temporery_label(app, text.get("0.0", "end-1c")))
+    #guess_number = int(text.get("0.0", "end-1c").strip())
+
+     
+
+    enter = ctk.CTkButton(app, text="Enter", command=lambda: 
+    temporery_label(app, check_guess(int(text.get("0.0", "end-1c").strip()) if text.get("0.0", "end-1c").strip().isdigit() else "Invalid Input", number)))
     enter.place(x=175, y=150)
 
     app.mainloop()
 
 if __name__ == "__main__":
     main()
-    
